d8d0b1874f01cc81ac47a46a3ef656e8e90a10e5
Brandon Lam <brandonlam4237@gmail.com>
2022-08-07 19:52:13 -0400

    qwerty keyboard layout

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -11,15 +11,47 @@ function Keyboard({ usedKeys }) {
   }, []);
   return (
     <div className="keyboard">
-      {letters &&
-        letters.map((letter) => {
-          const color = usedKeys[letter.key];
-          return (
-            <div key={letter.key} className={`${color} key`}>
-              {letter.key}
-            </div>
-          );
-        })}
+      <div className="keys r1">
+        {letters &&
+          letters.slice(0, 10).map((letter) => {
+            const color = usedKeys[letter.key];
+            return (
+              <div key={letter.key} className={`${color} key`}>
+                {letter.key}
+              </div>
+            );
+          })}
+      </div>
+      <div className="keys r2">
+        {letters &&
+          letters.slice(10, 19).map((letter) => {
+            const color = usedKeys[letter.key];
+            return (
+              <div key={letter.key} className={`${color} key`}>
+                {letter.key}
+              </div>
+            );
+          })}
+      </div>
+      <div className="keys r3">
+        {letters &&
+          letters.slice(19, 28).map((letter) => {
+            const color = usedKeys[letter.key];
+            if (letter.key === "enter" || letter.key === "del") {
+              console.log("enter or del");
+              return (
+                <div key={letter.key} className={`${color} key util`}>
+                  {letter.key}
+                </div>
+              );
+            }
+            return (
+              <div key={letter.key} className={`${color} key`}>
+                {letter.key}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
